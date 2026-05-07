4eebc58733b490e913a71404fe892944571db44b
ttty2436 <kimbnuuy0109@gmail.com>
2026-05-07 15:17:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
-st.title('하와이 5성급 호텔 이벤트 응모')
-st.text_input('이름을 입력해주세요')
-st.selectbox('사용하는 계좌 은행을 선택해주세요',['토스','우리은행','카카오뱅크','IBK'])
+st.title('하nn\ㅜㅜ와이 5성급 호텔 이벤트 응모')
+a=st.text_input('이름을 입력해주세요')
+b=st.selectbox('사용하는 계좌 은행을 선택해주세요',['토스','우리은행','카카오뱅크','IBK'])
+if st.button('공격말 생성'):
+  st.write(a+'님! 그대는 해킹 당햇스ㅅㅂ니다ㅜㅜ!')

d3ede358e534d5f85cc4473c23fd7eb2f8520883
ttty2436 <kimbnuuy0109@gmail.com>
2026-06-01 11:21:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.title('두근두근 다죽어월드 호텔 이벤트 응모')
+st.title('두근두 호텔 이벤트 응모')
 a=st.text_input('이름을 입력해주세요')
 b=st.selectbox('사용하는 계좌 은행을 선택해주세요',['토스','우리은행','카카오뱅크','IBK'])
 if st.button('응모 결과'):
-  st.write(a+'님! 그대는 해킹 당햇스ㅅㅂ니다ㅜㅜ!')
+  st.write(a+'님! 그대는 해킹 당햇스ㅅ니다ㅜㅜ!')
   st.info('이리 둔하실 수가')
-  st.warning(b+' 1조 앙 ㄱㅁㄸ!!')
-  st.error('gg ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋ ㅋㅋ ㅋ ㅋ ㅋ ㅋ ')
+  st.warning(b+' 1조 앙')
+  st.error('gg ')
   st.balloons()
